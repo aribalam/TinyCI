8794a7000aa4ba9ae7071fd9fdf9a2eb1365efc2
aribalam <arib.alam.iitkgp@gmail.com>
2020-07-06 19:13:44 +0530

    added loop for running bash script for updating repo

--- observer.py
+++ observer.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 
 
 def poll():
@@ -15,3 +16,11 @@ def poll():
 
     args = parser.parse_args()
     dispatcher_host, dispatcher_port = args.dispatcher_parser.split(":")
+
+    while True:
+        try:
+            # try calling a bash script to detect any changes in the repo
+            # Any new commit made will be added to a .commit_id file in the current working directory
+            subprocess.check_output(["./update_repo.sh", args.repo])
+        except subprocess.CalledProcessError as e:
+            raise Exception("Could not update and check repository: " + "Reason: %s" % e.output)
