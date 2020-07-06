6655199014bcd5da0993f7f50b1b974d86dc6b3c
aribalam <arib.alam.iitkgp@gmail.com>
2020-07-06 19:56:57 +0530

    extracts commits from origin repo

--- update_repo.sh
+++ update_repo.sh
@@ -17,3 +17,21 @@ if [ $? != 0 ]; then
 fi
 # Extract the commit id
 COMMIT_ID=$(echo $COMMIT | awk '{print $2}')
+
+# Extract commits from the origin repo
+git pull
+# get the most recent commit
+COMMIT=$(git log -n1)
+if [ $? != 0 ]; then
+  echo "Could not call git log"
+  exit 1
+fi
+# Extract the new commit id
+NEW_COMMIT_ID=$(echo $COMMIT | awk '{print $2}')
+
+# save the new commit to the file if both commit ids are different
+if [ COMMIT_ID != NEW_COMMIT_ID ]; then
+  popd 1> dev/null
+  echo $NEW_COMMIT_ID > .commit_id
+fi
+
