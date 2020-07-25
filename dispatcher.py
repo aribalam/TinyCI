1971b84d462cdb50566017d571aff0d31f266e2e
aribalam <arib.alam.iitkgp@gmail.com>
2020-07-25 14:10:07 +0530

    added custom handler for dispatcher server

--- dispatcher.py
+++ dispatcher.py
@@ -3,6 +3,8 @@ from socketserver import ThreadingTCPServer
 import socket
 import threading
 import time
+import re
+import os
 
 
 class ThreadingTCPServer(SocketServer.ThreadingMixin, SocketServer.TCPServer):
@@ -10,6 +12,65 @@ class ThreadingTCPServer(SocketServer.ThreadingMixin, SocketServer.TCPServer):
     dead = False
     dispatched_commits = {}
     pending_commits = []
+
+
+class DispatchHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our dispatcher.
+    This will dispatch test runners against the incoming commit
+    and handle their requests and test results
+    """
+    command_re = re.compile(r"(\w+)(:.+)*")
+    BUF_SIZE = 1024
+
+    def handle(self):
+        self.data = self.request.recv(self.BUF_SIZE).strip()
+        command_groups = self.command_re.math(self.data)
+        if not command_groups:
+            self.request.sendall("invalid data")
+            return
+        command = command_groups.group(1)
+
+        if command == "status":
+            print("In Status")
+            self.request.sendall("OK")
+
+        elif command == "register":
+            print("Register")
+            address = command_groups.group(2)
+            host, port = re.findall(r":(\w*)", address)
+            runner = {"host": host, "port": port}
+            self.server.runners.append(runner)
+            self.request.sendall("OK")
+
+        elif command == "dispatch":
+            print("going to dispatch")
+            commit_id = command_groups.group(2)[1:]
+            if not self.server.runners:
+                self.request.sendall("No runners are registered")
+            else:
+                self.request.sendall("OK")
+                dispatch_tests(self.server, commit_id)
+
+        elif command == "results":
+            print("got test results")
+            results = command_groups.group(2)[1:]
+            results = results.split(":")
+            commit_id = results[0]
+            length_msg = int(results[1])
+            # 3 is the number of ":" in the sent command
+            remaining_buffer = self.BUF_SIZE - (len(command) + len(commit_id) + len(results[1]) + 3)
+            if length_msg > self.BUF_SIZE:
+                self.data += self.request.recv(length_msg - remaining_buffer).strip()
+            del self.server.dispatched_commits[commit_id]
+
+            if not os.path.exists("test_results"):
+                os.makedirs("test_results")
+            with open("test_results/%s" % commit_id, "w") as f:
+                data = self.data.split(":")[3:]
+                data = "\n".join(data)
+                f.write(data)
+            self.request.sendall("OK")
 
 
 def dispatch_tests(server, commit_id):
